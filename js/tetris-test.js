9dbe5520b82c03172e4733e9131d8449c5d5342f
toxic1488 <kirillrad18@gmail.com>
2018-04-24 11:14:24 +0300

    -add glass grid

--- js/tetris-test.js
+++ js/tetris-test.js
@@ -2,10 +2,10 @@ var tetris = new Tetris();
 
 tetris.bindFigures(
 	{
-		"s":{
+		"z":{
 			form: "1,1,0;0,1,1"
 		},
-		"z":{
+		"s":{
 			form: "0,1,1;1,1,0"
 		},
 		"l":{
@@ -23,37 +23,47 @@ tetris.bindFigures(
 	}
 );
 
+tetris.startGame();
 
 window.onload = function(){
 
-	// function initRect( id, height, width){
-	// 	var rect = document.createElement('canvas');
-	// 	rect.id = id;
-	// 	rect.height = height;
-	// 	rect.width = width;
-	// 	rect.style = "border:1px solid #000000;";
-	// 	document.body.appendChild(rect);
-	// }
+	var canvas = document.createElement('canvas');
+	document.body.appendChild(canvas);
+	var ctx = canvas.getContext('2d');
+	canvas.height = 400;
+	canvas.width = 200;
+	var step = 20;
+	columns = tetris.GLASS_WIDTH
+	rows = tetris.GLASS_HEIGHT
+	var block_w = canvas.width / columns;
+	var block_h = canvas.height / rows;
 
-	// for (var i = 0; i < (tetris.GLASS_HEIGHT - 1)*(tetris.GLASS_HEIGHT - 1); i++) {
-	// 	initRect(i, 10, 10);
-	// }
+	function drawBlock( x, y){
+		ctx.fillRect(block_w * x, block_h * y, block_w - 1, block_h - 1);
+		ctx.strokeRect(block_w * x, block_h * y, block_w - 1, block_h - 1);
+	}
 
-	// function grid(width, height, span) {
-	// 	var cnv = document.createElement("canvas");
-	// 	cnv.width = width;
-	// 	cnv.height = height;
-	// 	var ctx = cnv.getContext("2d");
-	// 	ctx.strokeStyle = "red";
-	// 	var w = cnv.width - 1;
-	// 	var h = cnv.height - 1;
-	// 	for (var x = -0.5; x < w; x += span) ctx.strokeRect(x, 0, 0.1, h);
-	// 		for (var y = -0.5; y < h; y += span) ctx.strokeRect(0, y, w, 0.1);
-	// 			return cnv.toDataURL();
-	// }
+	function drawBoard(){
 
-	// grid(100,100, 10);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.strokeStyle = 'black';
+
+		for (var x = 0; x < columns; ++x) {
+			for (var y = 0; y < rows; ++y) {
+				//console.log(tetris.glass[y][x]);
+				if (tetris.glass[y][x] == 0){
+					ctx.fillStyle = 'white';
+					drawBlock( x, y);
+				}
+			}
+		}
+
+	}
+	drawBoard();
+	//setInterval( drawBoard, 30 )
+	
 }
+
 
 setInterval( gameStep, 40);
 function gameStep(){
--- js/tetris.js
+++ js/tetris.js
@@ -3,7 +3,7 @@ function Tetris(){
 	var scope = this;
 
 	var pressed_keys;
-	var glass;
+	scope.glass = [];
 	var state = "inactive";
 	var is_paused = false;
 	var figure_current;
@@ -24,11 +24,12 @@ function Tetris(){
 				_forms[i] = _forms[i].split(',');
 			}
 			
-			//console.log(_forms);
 			var array_of_figures = [_forms];
+
 			for (var i = 1; i < 4; i++) {
 				array_of_figures.push(transpose(array_of_figures[i - 1]))
 			}
+
 			figures[figure] = {
 				form: array_of_figures
 			};
@@ -42,11 +43,13 @@ function Tetris(){
 		state = "playing";
 		score = 0;
 		//reset glass
-		for (var i = 0; i < GLASS_HEIGHT - 1; i++) {
-			for (var j = 0; j < GLASS_HEIGHT - 1; j++) {
-				glass[i][j] = 0;
+		for (var i = 0; i < scope.GLASS_HEIGHT; i++) {
+			scope.glass[i] = [];
+			for (var j = 0; j < scope.GLASS_WIDTH; j++) {
+				scope.glass[i][j] = 0;
 			}
 		}
+		console.log(scope.glass);
 
 		//reset visual
 
@@ -79,13 +82,31 @@ function Tetris(){
 
 	}
 
-	function transpose( matrix){
+	function moveFigure( x, y ){
+
+	}
+
+	function dropFigure(){
+
+	}
+
+	function figureToGlass(){
+
+	}
+
+	function checkFilledRows(){
+
+	}
+
+	function gameOver(){
+
+	}
+
+	function transpose( matrix ){
 
 		var copy = [];
 		for (var i = 0; i < matrix.length; ++i) {
 			for (var j = 0; j < matrix[i].length; ++j) {
-				// skip undefined values to preserve sparse array
-				//if (matrix[i][j] === undefined) continue;
 				// create row if it doesn't exist yet
 				if (copy[j] === undefined) copy[j] = [];
 				// swap the x and y coords for the copy
